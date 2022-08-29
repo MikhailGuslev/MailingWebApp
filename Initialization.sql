1195e966deddc60841c5ff8bc75834c07e560b9f
MikhailGuslev <m.guslev@s-procenter.ru>
2022-08-29 15:11:29 +0300

    feat: Add db file and db initialization script

---
+++ Initialization.sql
@@ -0,0 +1,56 @@
+﻿create table 'User'
+(
+    UserId integer not null,
+    Email text not null
+);
+
+create table Plugin
+(
+    PluginId integer not null,
+    Name text not null,
+    Comment text not null,
+    Data blob not null,
+    CreatedDate datetime not null,
+    UpdatedDate datetime not null
+);
+
+create table ModelProvider
+(
+    ModelProviderId integer not null,
+    ModelProviderTypeName text not null,
+
+    PluginId integer not null
+);
+
+create table MessageTemplate
+(
+    MessageTemplateId integer not null,
+    Subject text not null,
+    Body text not null,
+    ContentType text not null,
+    IsBodyStatic boolean not null,
+    IsSubjectStatic boolean not null,
+
+    ModelProviderId integer null,
+
+    check (ContentType in ("PlainText", "Html"))
+);
+
+create table EmailSending
+(
+    SendingId integer not null,
+    Name text not null,
+    Recipients text not null,
+
+    MessageTemplateId integer not null
+);
+
+create table EmailSendingSchedule
+(
+    EmailSendingScheduleId integer not null,
+    ActivationTimePoint datetime not null,
+    DeactivationTimePoint datetime not null,
+    ActivationInterval integer not null,
+
+    EmailSendingId integer not null
+);
