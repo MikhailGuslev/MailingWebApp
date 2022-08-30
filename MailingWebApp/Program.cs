008176a7ca3c664a47ea7aa526f354d2eb2dcf8a
MikhailGuslev <m.guslev@s-procenter.ru>
2022-08-30 08:56:27 +0300

    refactor: Refactor services registration

--- MailingWebApp/Program.cs
+++ MailingWebApp/Program.cs
@@ -1,11 +1,9 @@
-using DataLayer;
 using DataLayer.Entities;
 using LinqToDB.AspNet;
 using LinqToDB.Configuration;
 using Mailing;
-using Mailing.Abstractions;
 using Mailing.Settings;
-using MailingWebApp.Repositories;
+using MailingWebApp.Infrastructure;
 using NLog.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -30,17 +28,11 @@ builder.Services
     );
 
 builder.Services
-    .AddTransient<StorageInitializer>()
-    .AddScoped<IMessageModelProviderRepository, FakeMessageModelProviderRepository>()
-    .AddScoped<IEmailSendingRepository, FakeEmailSendingRepository>()
+    .AddScopedRepositories()
+    .AddHostedService<StorageInitializerHostedService>()
     .AddHostedService<MailingBackgroundService>();
 
 var app = builder.Build();
-
-using (IServiceScope initScope = app.Services.CreateScope())
-{
-    initScope.ServiceProvider.GetRequiredService<StorageInitializer>();
-}
 
 if (app.Environment.IsDevelopment())
 {
