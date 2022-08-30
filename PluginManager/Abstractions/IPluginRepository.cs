773a8d8b6d99cb868e63c4b0ccbd3522f83e8f2a
MikhailGuslev <m.guslev@s-procenter.ru>
2022-08-30 20:58:02 +0300

    style: Add to IPluginRepository nullable notation

--- PluginManager/Abstractions/IPluginRepository.cs
+++ PluginManager/Abstractions/IPluginRepository.cs
@@ -4,8 +4,8 @@ namespace PluginManager.Abstractions;
 
 public interface IPluginRepository
 {
-    Task<PluginInformation> GetPluginInformationAsync(int pluginId);
-    Task<Plugin> GetPluginAsync(int pluginId);
-    Task<IReadOnlyList<Plugin>> GetAllPluginsAsync();
+    Task<PluginInformation?> GetPluginInformationAsync(int pluginId);
+    Task<Plugin?> GetPluginAsync(int pluginId);
+    Task<IReadOnlyList<Plugin>?> GetAllPluginsAsync();
     Task AddPluginAsync(Plugin plugin);
 }
