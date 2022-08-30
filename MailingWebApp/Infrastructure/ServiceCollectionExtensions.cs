9eea3a1ed755f9a0b0d4ccd290232bfc8ad0c671
MikhailGuslev <m.guslev@s-procenter.ru>
2022-08-30 08:55:09 +0300

    feat: Add AddScopedRepositories extension method

---
+++ MailingWebApp/Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Mailing.Abstractions;
+using MailingWebApp.Repositories;
+
+namespace MailingWebApp.Infrastructure;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddScopedRepositories(this IServiceCollection services)
+    {
+        services
+            .AddScoped<IMessageModelProviderRepository, FakeMessageModelProviderRepository>()
+            .AddScoped<IEmailSendingRepository, FakeEmailSendingRepository>()
+            .AddScoped<FakeUserRepository>()
+            .AddScoped<FakeRecipientRepository>();
+
+        return services;
+    }
+}
