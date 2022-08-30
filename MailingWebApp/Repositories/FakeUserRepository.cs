050c1d231d31ec4286c008f9418fa89ac0d47c64
MikhailGuslev <m.guslev@s-procenter.ru>
2022-08-30 08:54:48 +0300

    feat: Add FakeUserRepository

---
+++ MailingWebApp/Repositories/FakeUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using DataLayer.Entities;
+using LinqToDB;
+
+namespace MailingWebApp.Repositories;
+
+public sealed class FakeUserRepository
+{
+    private readonly StorageDb Storage;
+
+    public FakeUserRepository(StorageDb storage)
+    {
+        Storage = storage;
+    }
+
+    public async Task<IReadOnlyList<User>> GetAllUsersAsync()
+    {
+        return await Storage.User.ToListAsync();
+    }
+}
