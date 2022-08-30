ed6562c716ce059f1ed9fa680b66fd80ca253f2b
MikhailGuslev <m.guslev@s-procenter.ru>
2022-08-30 08:54:38 +0300

    feat: Add FakeRecipientRepository

---
+++ MailingWebApp/Repositories/FakeRecipientRepository.cs
@@ -0,0 +1,40 @@
+﻿using Mailing.Models;
+
+namespace MailingWebApp.Repositories;
+
+public sealed class FakeRecipientRepository
+{
+    private const char Separator = ',';
+    private readonly FakeUserRepository UserRepository;
+
+    public FakeRecipientRepository(FakeUserRepository userRepository)
+    {
+        UserRepository = userRepository;
+    }
+
+    public async Task<IReadOnlyList<Recipient>> GetAllRecipientsAsync()
+    {
+        IReadOnlyList<DataLayer.Entities.User> users = await UserRepository
+            .GetAllUsersAsync();
+
+        return users
+            .Select(x => new Recipient { UserId = x.UserId, Email = x.Email })
+            .ToList();
+    }
+    public async Task<IReadOnlyList<Recipient>> GetRecipientsFromStringAsync(string recipientIdsAsString)
+    {
+        long[] ids = recipientIdsAsString
+            .Split(Separator)
+            .Select(x => long.TryParse(x, out long value) ? value : default)
+            .Where(x => x != default)
+            .ToArray();
+
+        IReadOnlyList<DataLayer.Entities.User> users = await UserRepository
+            .GetAllUsersAsync();
+
+        return users
+            .Where(x => ids.Contains(x.UserId))
+            .Select(x => new Recipient { UserId = x.UserId, Email = x.Email })
+            .ToList();
+    }
+}
