ef0e82ae2bfe59c0ae68129d61a33b5281ce6ca8
MikhailGuslev <m.guslev@s-procenter.ru>
2022-08-31 20:45:38 +0300

    feat: Add EmailSendingSchedule.Validate And LastActivation

--- Mailing/Models/EmailSendingSchedule.cs
+++ Mailing/Models/EmailSendingSchedule.cs
@@ -7,7 +7,43 @@ public sealed record class EmailSendingSchedule
 {
     public int EmailSendingScheduleId { get; init; }
     public EmailSending EmailSending { get; init; } = new();
-    public DateTime ActivationTimePoint { get; init; }
-    public DateTime DeactivationTimePoint { get; init; }
-    public TimeSpan ActivationInterval { get; init; }
+    public DateTime ActivationTimePoint { get; init; } = DateTime.MinValue;
+    public DateTime DeactivationTimePoint { get; init; } = DateTime.MinValue;
+    public TimeSpan ActivationInterval { get; init; } = TimeSpan.Zero;
+
+    public DateTime LastActivation { get; private set; } = DateTime.MinValue;
+
+    public void Activate()
+    {
+        LastActivation = DateTime.Now;
+    }
+
+    public DateTime? GetNextActivation()
+    {
+        if (DateTime.Now >= DeactivationTimePoint) return null;
+
+        TimeSpan elapsed = DateTime.Now - LastActivation;
+
+        if (elapsed > ActivationInterval)
+        {
+            // NOTE: актуализация времени последней активации
+            (long countActivation, long remainder) = Math
+                .DivRem(elapsed.Ticks, ActivationInterval.Ticks);
+
+            LastActivation += countActivation * ActivationInterval;
+            elapsed = DateTime.Now - LastActivation;
+        }
+
+        DateTime maybeNext = LastActivation + ActivationInterval - elapsed;
+
+        return maybeNext >= DeactivationTimePoint
+            ? null
+            : maybeNext;
+    }
+
+    public bool Validate()
+    {
+        return ActivationTimePoint < DeactivationTimePoint
+            && (ActivationTimePoint + ActivationInterval) < DeactivationTimePoint;
+    }
 }
