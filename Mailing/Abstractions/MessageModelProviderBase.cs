ebf7f65cb33bd4f90dc3c5c2035971ea5dd1d77e
MikhailGuslev <m.guslev@s-procenter.ru>
2022-08-31 10:51:28 +0300

    feat: Add MessageModelProviderBase

---
+++ Mailing/Abstractions/MessageModelProviderBase.cs
@@ -0,0 +1,16 @@
+﻿using Mailing.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mailing.Abstractions;
+
+public abstract class MessageModelProviderBase : IMessageModelProvider
+{
+    public MessageModelProviderBase(IServiceScopeFactory serviceScopeFactory)
+    {
+        ServiceScopeFactory = serviceScopeFactory;
+    }
+
+    public IServiceScopeFactory ServiceScopeFactory { get; private set; }
+
+    public abstract Task<IMessageModel> GetModelAsync(Recipient recipient);
+}
