89a67431f0ee84782b708dfd0d59d1e50e78e0da
MikhailGuslev <m.guslev@s-procenter.ru>
2022-08-31 08:04:09 +0300

    style: Add nullable annotation

--- Mailing/Abstractions/IMessageModelProviderRepository.cs
+++ Mailing/Abstractions/IMessageModelProviderRepository.cs
@@ -2,8 +2,8 @@
 
 public interface IMessageModelProviderRepository
 {
-    Task<IMessageModelProvider> GetMessageModelProviderAsync(Type providerType);
-    Task<IMessageModelProvider> GetMessageModelProviderAsync(string providerTypeName);
+    Task<IMessageModelProvider?> GetMessageModelProviderAsync(Type providerType);
+    Task<IMessageModelProvider?> GetMessageModelProviderAsync(string providerTypeName);
 
     Task AddMessageModelProviderAsync(IMessageModelProvider modelProvider);
 }
