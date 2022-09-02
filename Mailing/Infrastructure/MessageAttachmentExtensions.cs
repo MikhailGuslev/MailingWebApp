f135dd29bec62ef9ed698a101b1d936f286e0c85
MikhailGuslev <m.guslev@s-procenter.ru>
2022-09-02 14:48:45 +0300

    feat: Add MapToMimeEntity method extension

---
+++ Mailing/Infrastructure/MessageAttachmentExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Mailing.Models;
+using MimeKit;
+
+namespace Mailing.Infrastructure;
+
+public static class MessageAttachmentExtensions
+{
+    public static MimeEntity MapToMimeEntity(this MessageAttachment original)
+    {
+        MemoryStream contentDataStream = new(original.Content);
+
+        MimePart mimeEntity = new(original.ContentType)
+        {
+            ContentId = original.MessageAttachmentId,
+            Content = new MimeContent(contentDataStream, ContentEncoding.Default),
+            ContentDisposition = new ContentDisposition(ContentDisposition.Attachment),
+            ContentTransferEncoding = ContentEncoding.Base64,
+            FileName = Path.GetFileName(original.OriginalContentName)
+        };
+
+        return mimeEntity;
+    }
+}
