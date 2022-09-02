996d9cfc9788e94c68e507895120584bf9f17136
MikhailGuslev <m.guslev@s-procenter.ru>
2022-09-02 14:35:09 +0300

    feat: Add MessageAttachment

---
+++ Mailing/Models/MessageAttachment.cs
@@ -0,0 +1,9 @@
+﻿namespace Mailing.Models;
+
+public sealed record class MessageAttachment
+{
+    public string MessageAttachmentId { get; init; } = string.Empty;
+    public string OriginalContentName { get; init; } = string.Empty;
+    public string ContentType { get; init; } = string.Empty;
+    public byte[] Content { get; init; } = Array.Empty<byte>();
+}
