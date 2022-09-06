fdafbc92b18e0bb1ac25f73d814886b2447d688d
MikhailGuslev <m.guslev@s-procenter.ru>
2022-09-06 20:48:52 +0300

    refactor: Remove abstractions and models from PluginManager proj

--- PluginManager/Models/InstanceCreationOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PluginManager.Models;
-
-public sealed record class InstanceCreationOptions
-{
-    public int PluginId { get; init; }
-    public string PluggableTypeName { get; init; } = string.Empty;
-    public object[] ConstructorArgumets { get; init; } = Array.Empty<object>();
-    public Type? InterfaceType { get; init; }
-}
