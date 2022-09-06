a57e51419bbc132404e89d0c4c7f875ca43ac0d4
MikhailGuslev <m.guslev@s-procenter.ru>
2022-09-06 20:52:18 +0300

    feat: Add PluginManagerException

---
+++ PluginManager/Infrastructure/PluginManagerException.cs
@@ -0,0 +1,9 @@
+﻿namespace PluginManager.Infrastructure;
+
+public sealed class PluginManagerException : Exception
+{
+    public PluginManagerException(string message, Exception? innerException = null)
+        : base(message, innerException)
+    {
+    }
+}
