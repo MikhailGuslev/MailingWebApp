a24560cdc1e7de87fd2719106b0129cf6d8e4165
MikhailGuslev <m.guslev@s-procenter.ru>
2022-09-06 20:52:06 +0300

    feat: Add PluginAssemblyExtensions

---
+++ PluginManager/Infrastructure/PluginAssemblyExtensions.cs
@@ -0,0 +1,21 @@
+﻿using PluginManager.Models;
+
+namespace PluginManager.Infrastructure;
+
+public static class PluginAssemblyExtensions
+{
+    public static PluginAssemblyInformation GetInformation(this PluginAssembly pluginAssembly)
+    {
+        ArgumentNullException.ThrowIfNull(pluginAssembly, nameof(pluginAssembly));
+
+        return new PluginAssemblyInformation
+        {
+            PluginId = pluginAssembly.PluginAssemblyId,
+            Name = pluginAssembly.Name,
+            Comment = pluginAssembly.Comment,
+            Settings = pluginAssembly.Settings,
+            CreatedDate = pluginAssembly.CreatedDate,
+            UpdatedDate = pluginAssembly.UpdatedDate,
+        };
+    }
+}
