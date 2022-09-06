64282d7269eeb081f315d143bcb67883f6813dc1
MikhailGuslev <m.guslev@s-procenter.ru>
2022-09-06 20:50:53 +0300

    refactor: Move all PluginManager models to PluginManager.Models

---
+++ PluginManager.Models/PluginAssembly.cs
@@ -0,0 +1,12 @@
+﻿namespace PluginManager.Models;
+
+public sealed record class PluginAssembly
+{
+    public int PluginAssemblyId { get; init; }
+    public string Name { get; init; } = string.Empty;
+    public string Comment { get; init; } = string.Empty;
+    public string Settings { get; init; } = string.Empty;
+    public byte[] Data { get; init; } = Array.Empty<byte>();
+    public DateTime CreatedDate { get; init; }
+    public DateTime UpdatedDate { get; init; }
+}
---
+++ PluginManager.Models/PluginAssemblyInformation.cs
@@ -0,0 +1,11 @@
+﻿namespace PluginManager.Models;
+
+public sealed record class PluginAssemblyInformation
+{
+    public int PluginId { get; init; }
+    public string Name { get; init; } = string.Empty;
+    public string Comment { get; init; } = string.Empty;
+    public string Settings { get; init; } = string.Empty;
+    public DateTime CreatedDate { get; init; }
+    public DateTime UpdatedDate { get; init; }
+}
